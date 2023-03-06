fae136e2365817c57dd16b68389b157f4d95c10b
acheronfail <acheronfail@gmail.com>
2023-03-07 08:19:27 +1030

    wip: read a string from openat

--- src/main.rs
+++ src/main.rs
@@ -68,21 +68,79 @@
 //! [`strace`]: https://strace.io/
 //! [tracefile]: https://gitlab.com/ole.tange/tangetools/tree/master/tracefile
 
-use std::{collections::HashMap, os::unix::process::CommandExt, process::Command};
+use std::{collections::HashMap, os::unix::process::CommandExt, process::Command, ffi::c_void};
 
+use byteorder::{LittleEndian, WriteBytesExt};
 use nix::{
+    libc::{user_regs_struct, c_long, AT_FDCWD},
     sys::{ptrace, wait::waitpid},
     unistd::Pid,
 };
 use owo_colors::OwoColorize;
 
+fn print_syscall(syscall_table: &HashMap<u64, String>, child_pid: Pid, regs: user_regs_struct, width: usize) {
+
+    if regs.orig_rax != 257 {
+        return
+    }
+
+    // TODO: read enums: how to know?
+    eprintln!("{:x}", AT_FDCWD);
+
+    // TODO: read string arg; make this generic across calls
+    if regs.orig_rax == 257 {
+        let mut s = String::new();
+        let mut count = 0;
+        let word_size = 8;
+
+        let addr = regs.rsi as *mut c_void;
+
+        'read_string: loop {
+            let mut bytes = vec![];
+            let addr = unsafe { addr.offset(count) };
+
+            let res: c_long;
+
+            match ptrace::read(child_pid, addr) {
+                Ok(c_long) => res = c_long,
+                Err(_) => break 'read_string,
+            }
+
+            bytes.write_i64::<LittleEndian>(res).unwrap(); // TODO: handle
+
+            for b in bytes {
+                if b != 0 {
+                    s.push(b as char);
+                } else {
+                    break 'read_string;
+                }
+            }
+
+            count += word_size;
+        }
+
+        eprintln!("{:?}", s.red());
+    }
+
+    eprintln!(
+        "{:>width$}({:x}, {:x}, {:x}, ...) = {:x}",
+        syscall_table[&regs.orig_rax].bright_green(),
+        regs.rdi.cyan(),
+        regs.rsi.cyan(),
+        regs.rdx.cyan(),
+        regs.rax.yellow(),
+        width = width
+    );
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let syscall_table: HashMap<u64, String> = serde_json::from_str(include_str!("syscall.json"))?;
-    let longest_syscall_name = syscall_table
-        .iter()
-        .map(|(_, value)| value.len())
-        .max_by(|a, b| usize::cmp(a, b))
-        .unwrap();
+    let longest_syscall_name = 3;
+    // let longest_syscall_name = syscall_table
+    //     .iter()
+    //     .map(|(_, value)| value.len())
+    //     .max_by(|a, b| usize::cmp(a, b))
+    //     .unwrap();
 
     // TODO: argument parsing
 
@@ -99,8 +157,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let child = cmd.spawn()?;
     let child_pid = Pid::from_raw(child.id() as _);
 
-    let res = waitpid(child_pid, None)?;
-    eprintln!("waitpid: {:?}", res.yellow());
+    _ = waitpid(child_pid, None)?;
+    print_syscall(
+        &syscall_table,
+        child_pid,
+        ptrace::getregs(child_pid)?,
+        longest_syscall_name,
+    );
 
     let mut is_sys_exit = false;
     loop {
@@ -113,16 +176,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             };
 
             // TODO: argument parsing of syscall functions that take files
-
-            eprintln!(
-                "{:>width$}({:x}, {:x}, {:x}, ...) = {:x}",
-                syscall_table[&regs.orig_rax].bright_green(),
-                regs.rdi.cyan(),
-                regs.rsi.cyan(),
-                regs.rdx.cyan(),
-                regs.rax.yellow(),
-                width = longest_syscall_name
-            );
+            print_syscall(&syscall_table, child_pid, regs, longest_syscall_name);
         }
         is_sys_exit = !is_sys_exit;
     }
